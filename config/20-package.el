994b982cd42985dcc1f35e77f6a5be3f82c56d0c
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-02-28 23:00:07 +0900

    Add package config

---
+++ config/20-package.el
@@ -0,0 +1,29 @@
+;;
+;; package.el
+;;
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("ELPA" . "https://tromey.com/elpa/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+;; (("ELPA" . "http://tromey.com/elpa/")
+;;  ("gnu" . "https://elpa.gnu.org/packages/")
+;;  ("melpa" . "https://melpa.org/packages/")
+;;  ("org" . "http://orgmode.org/elpa/"))
+
+
+;; (when (require 'package nil t)
+;;   ;; (setq package-user-dir "~/.emacs.d/package/elpa/")
+;;   ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;;   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+;;   ;; インストールしたパッケージにロードパスを通してロードする
+;;   (package-initialize))
+
+;; (require 'package nil t)
+;; (setq package-user-dir "~/.emacs.d/package/elpa/")
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+;; ;; インストールしたパッケージにロードパスを通してロードする
+;; (package-initialize)
