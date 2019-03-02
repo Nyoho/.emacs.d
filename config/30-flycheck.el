1bbb898381de5e0b3023c896209b78fe9af0e825
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-03-03 06:12:46 +0900

    Add config of flycheck

---
+++ config/30-flycheck.el
@@ -0,0 +1,23 @@
+(use-package flycheck
+  :defer t
+  :config
+  ;; (add-hook 'after-init-hook #'global-flycheck-mode)
+
+  ;; (flycheck-def-option-var flycheck-d-custom-import-paths nil d-dmd
+  ;;   "A list of additional import paths."
+  ;;   :type '(repeat :tag "Additional paths"
+  ;;           (string :tag "import path"))
+  ;;   :safe #'flycheck-string-list-p
+  ;;   :package-version '(flycheck . "0.14"))
+
+  ;; (put 'd-dmd :flycheck-command
+  ;;      (append (flycheck-checker-command 'd-dmd)
+  ;;              '((option-list "-I" flycheck-d-custom-import-paths s-prepend))))
+
+  ;; flycheck-pos-tip
+  (flycheck-pos-tip-mode)
+  (setq flycheck-pos-tip-timeout 30))
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
