c2346cc74028414a802801e0ac7a1a0cefcd8264
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-03-17 07:05:48 +0900

    Add anzu

---
+++ config/50-anzu.el
@@ -0,0 +1,16 @@
+;; 検索置換が簡単になる
+(with-eval-after-load "postpone"
+  (use-package anzu
+    :init
+    (global-anzu-mode +1)
+    (setq anzu-use-migemo t)
+    (setq anzu-search-threshold 1000)
+    (setq anzu-minimum-input-length 3)
+    :config
+
+    :bind (("C-c r" . anzu-query-replace)
+           ("C-c R" . anzu-query-replace-regexp)))
+
+  ;; (global-set-key (kbd "C-c r") 'anzu-query-replace)
+  ;; (global-set-key (kbd "C-c R") 'anzu-query-replace-regexp)
+  )
