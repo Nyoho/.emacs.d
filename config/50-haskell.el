60bd0fe29f1395c7f0118c487ea01c7f847aac0f
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-03-24 07:30:54 +0900

    Add config of Haskell

---
+++ config/50-haskell.el
@@ -0,0 +1,7 @@
+;;
+;; Haskell-mode
+;;
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook #'hindent-mode)
