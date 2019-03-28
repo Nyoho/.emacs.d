6d6bdffcf0f49a97f687c56dc9ad1d20bea20bfb
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-03-28 18:50:02 +0900

    Add settings for editing

---
+++ config/50-editing.el
@@ -0,0 +1,45 @@
+;;; editing
+
+(use-package expand-region
+  :config
+  (global-set-key (kbd "C-,") 'er/expand-region)
+  (global-set-key (kbd "C-M-,") 'er/contract-region))
+
+(use-package multiple-cursors :defer t)
+(use-package smartrep
+  :config
+  (smartrep-define-key
+      global-map "C-." '(("C-n" . 'mc/mark-next-like-this)
+                         ("C-p" . 'mc/mark-previous-like-this)
+                         ("*"   . 'mc/mark-all-like-this))))
+
+;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+;; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(global-set-key (kbd "<C-M-return>") 'mc/edit-lines)
+
+;; region を作って M-x mc/mark-all-like-this すると同時編集
+;; ここで練習してみる↓
+
+;; abcd
+;; bcde
+;; cdef
+
+;; let a = 1
+;; let b = 2
+;; let c = 3
+
+(use-package outline)
+(use-package outline-magic) ;; org-modeのように折り畳みができる。
+
+(add-hook 'outline-mode-hook
+          (lambda ()
+            ))
+
+(add-hook 'outline-minor-mode-hook
+          (lambda ()
+            (use-package outline-magic)
+            (define-key outline-minor-mode-map [(f10)] 'outline-cycle)))
+
