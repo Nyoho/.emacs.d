40a5cbe9da7746c105c37f6703e9cca6ee66acf6
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-04-08 08:32:20 +0900

    Add ein (Jupyter Notebook interface)

---
+++ config/50-ein.el
@@ -0,0 +1,9 @@
+;;
+;; ein: Jupyter notebook client
+;; https://github.com/millejoh/emacs-ipython-notebook
+;;
+
+(setq ein:jupyter-default-server-command (concat (getenv "HOME") "/.pyenv/shims/jupyter"))
+(setq ein:jupyter-default-notebook-directory (concat (getenv "HOME") "/prog"))
+
+;; then call ein:jupyter-server-start.
