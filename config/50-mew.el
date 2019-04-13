086a2c2c9bac17262815990ac2eeec808c77bf67
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-04-14 08:31:29 +0900

    Add mew setting
    もうほとんど使ってないけど

---
+++ config/50-mew.el
@@ -0,0 +1,27 @@
+;;
+;; Mew, a mailer
+;;
+(defun fetchmail ()
+  (interactive)
+  (shell-command "env LANG=C fetchmail&"))
+
+(autoload 'mew "mew" nil t)
+(autoload 'mew-send "mew" nil t)
+
+;; (setq mew-icon-directory "icon directory") ;; if using XEmacs/Emacs 21
+
+;; Optional setup (Read Mail menu for Emacs 21):
+(if (boundp 'read-mail-command)
+    (setq read-mail-command 'mew))
+
+;; Optional setup (e.g. C-xm for sending a message):
+(autoload 'mew-user-agent-compose "mew" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'mew-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'mew-user-agent
+      'mew-user-agent-compose
+      'mew-draft-send-message
+      'mew-draft-kill
+      'mew-send-hook))
