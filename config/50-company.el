37d8ce0b1c0c9a4940ac0915129721b42a393f3d
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-04-24 21:04:03 +0900

    Add company setting

---
+++ config/50-company.el
@@ -0,0 +1,24 @@
+
+(use-package company
+  :init
+  (global-company-mode +1)
+  :custom
+  (completion-ignore-case t)
+  (company-idle-delay 0)
+  (company-selection-wrap-around t)
+  :config
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "C-i") 'company-complete-selection)
+  (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+  )
+
+(use-package company-box
+  :after company
+  :hook (company-mode . company-box-mode)
+  :config
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons)
+  )
