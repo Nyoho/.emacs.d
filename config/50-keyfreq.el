31df63681904182e58fa92b1fe6e960c6aff3c33
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-04-26 08:41:03 +0900

    Add keyfreq setting
    Thanks takaxp: https://twitter.com/takaxp/status/1067377916722393088

---
+++ config/50-keyfreq.el
@@ -0,0 +1,10 @@
+;; keyfreq.el
+;;
+;; A statistics for history of commands I execute
+;; cf. https://twitter.com/takaxp/status/1067377916722393088
+(use-package keyfreq
+  :config
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1)
+  )
+
