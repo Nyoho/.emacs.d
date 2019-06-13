411387f0a1b690f340bd8bb75880d024d5f10f9c
北䑓如法 <algebraicallyClosedField@gmail.com>
2019-06-13 23:08:54 +0900

    modify ein setting

--- config/50-ein.el
+++ config/50-ein.el
@@ -5,5 +5,5 @@
 
 (setq ein:jupyter-default-server-command (concat (getenv "HOME") "/.pyenv/shims/jupyter"))
 (setq ein:jupyter-default-notebook-directory (concat (getenv "HOME") "/prog"))
-
+(setq ein:notebook-mode nil) ;; ?
 ;; then call ein:jupyter-server-start.
