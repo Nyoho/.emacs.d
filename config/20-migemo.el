66faac4f67356bff08eeb2f44ca5cd6d1574c603
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-04-21 07:20:49 +0900

    leafed: migemo

--- config/20-migemo.el
+++ config/20-migemo.el
@@ -2,7 +2,7 @@
 ;; for migemo -- migemo 最高
 ;;
 ;; (if (equal (call-process-shell-command "which cmigemo" nil t) 0)
-(use-package migemo
+(leaf migemo
   :ensure t
   :when (executable-find "cmigemo")
   
@@ -12,20 +12,20 @@
   ;; (setq migemo-options '("-q" "--emacs" "-i" "\g"))
   ;; (setq migemo-dictionary "/usr/local/share/migemo/euc-jp/migemo-dict")
   :custom
-  (migemo-command "cmigemo")
-  (migemo-user-dictionary nil)
-  (migemo-regex-dictionary nil)
-  (migemo-options '("-q" "--emacs"))
-  (migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
-  (migemo-coding-system 'utf-8-unix)
+  ((migemo-command . "cmigemo")
+   (migemo-user-dictionary . nil)
+   (migemo-regex-dictionary . nil)
+   (migemo-options . '("-q" "--emacs"))
+   (migemo-dictionary . "/usr/local/share/migemo/utf-8/migemo-dict")
+   (migemo-coding-system . 'utf-8-unix))
   :config
   (load-library "migemo")
   (migemo-init)
 
-  (use-package avy-migemo
+  (leaf avy-migemo
     :disabled
     :after swiper
     :config
     ;; (avy-migemo-mode 1) ;; Emacs headだと動くらしい2019年6月26日(水) https://github.com/abo-abo/swiper/issues/1961
-    (require 'avy-migemo-e.g.swiper))
+    (leaf avy-migemo-e.g.swiper))
   )
