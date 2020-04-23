d1fb3b3817835abcb72e535f74de8dc36155c98a
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-04-23 20:00:22 +0900

    leafed: company*

--- config/50-company.el
+++ config/50-company.el
@@ -1,23 +1,33 @@
 
-(use-package company
+(leaf company
   :ensure t
   :init
   (global-company-mode +1)
   :custom
-  (completion-ignore-case t)
-  (company-idle-delay 0)
-  (company-selection-wrap-around t)
+  (completion-ignore-case . t)
+  (company-idle-delay . 0)
+  (company-selection-wrap-around . t)
+  :bind
+  ((company-active-map ("C-n" . company-select-next))
+   (company-active-map ("C-p" . company-select-previous))
+   (company-search-map ("C-n" . company-select-next))
+   (company-search-map ("C-p" . company-select-previous))
+   (company-active-map ("C-s" . company-filter-candidates))
+   (company-active-map ("C-i" . company-complete-selection))
+   (emacs-lisp-mode-map ("C-M-i" . company-complete)))
+  :require t
   :config
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-search-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-  (define-key company-active-map (kbd "C-i") 'company-complete-selection)
-  (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+  (leaf company-auctex
+    :ensure t
+    :after auctex
+    :init (company-auctex-init))
+
+  (leaf company-math
+    :ensure t
+    :after auctex org)
   )
 
-(use-package company-box
+(leaf company-box
   :ensure t
   :after company
   :hook (company-mode . company-box-mode)
