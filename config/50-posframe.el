218f0cb43e2410e04e6d761c5283e9152ff2032d
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-04-29 08:30:12 +0900

    leafed: posframe

--- config/50-posframe.el
+++ config/50-posframe.el
@@ -1,16 +1,20 @@
-(use-package posframe
+(leaf posframe
   :ensure t
   :when window-system
   :config
-  (use-package ivy-posframe
+  (leaf ivy-posframe
     :ensure t
-    :if window-system
+    :when window-system
+    :hook
+    (ivy-mode . ivy-posframe-enable)
+    :config
+    (ivy-posframe-mode)
     :custom
-    (ivy-display-function #'ivy-posframe-display-at-frame-center)
-    (ivy-posframe-parameters '((left-fringe . 4)
-                               (right-fringe . 4)
-                               (internal-border-width . 8)
-                               (internal-border-color . "white")))
+    (ivy-display-function . #'ivy-posframe-display-at-frame-center)
+    (ivy-posframe-parameters . '((left-fringe . 4)
+                                 (right-fringe . 4)
+                                 (internal-border-width . 8)
+                                 (internal-border-color . "white")))
     (ivy-posframe-display-functions-alist
      '((swiper          . nil)
        (complete-symbol . ivy-posframe-display-at-point)
@@ -18,10 +22,7 @@
        (counsel-M-x     . ivy-posframe-display-at-window-center)
        (t               . ivy-posframe-display)))
     :custom-face
-    (ivy-posframe ((t (:background "#2B5B7A"))))
-    (ivy-posframe-border ((t (:background "#7272a4"))))
-    (ivy-posframe-cursor ((t (:background "#a0bfff"))))
-    :hook
-    (ivy-mode . ivy-posframe-enable)
-    :init
-    (ivy-posframe-mode)))
+    (ivy-posframe        . '((t (:background "#2B5B7A"))))
+    (ivy-posframe-border . '((t (:background "#7272a4"))))
+    (ivy-posframe-cursor . '((t (:background "#a0bfff"))))))
+
