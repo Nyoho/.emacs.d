2790dafeaf9f95c535c74c1ae6d2843de0aba056
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-05-01 18:52:28 +0900

    leafed: rust

--- config/50-rust.el
+++ config/50-rust.el
@@ -1,28 +1,30 @@
 ;; From http://keens.github.io/blog/2016/12/29/kizuitararustnokankyoukouchikugakanarirakuninatteta/
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-(eval-after-load "rust-mode"
-  '(setq-default rust-format-on-save t))
-(add-hook 'rust-mode-hook (lambda ()
-                            (racer-mode)
-                            (flycheck-rust-setup)))
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook (lambda ()
-                             (company-mode)
-                             (set (make-variable-buffer-local 'company-idle-delay) 0.3)
-                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 1)))
-
 ;; rustup component add rust-src しておく
 ;; rustup default nightly
 
-(defun add-hook-rust-test ()
-  (interactive)
-  (add-hook 'after-save-hook (lambda ()
-                               (rust-test))))
-(defun run-rust-test ()
-  (interactive)
-  (rust-test))
+(leaf rust
+  :hook ((rust-mode-hook . (lambda ()
+                             (racer-mode)
+                             (flycheck-rust-setup)))
+         (racer-mode-hook . eldoc-mode)
+         (racer-mode-hook . (lambda ()
+                              (company-mode)
+                              (set (make-variable-buffer-local 'company-idle-delay)  0.3)
+                              (set (make-variable-buffer-local 'company-minimum-prefix-length) 1))))
+  :custom ((rust-format-on-save . t))
+  :config
+  (add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
 
-(use-package rust
+  (defun add-hook-rust-test nil
+    (interactive)
+    (add-hook 'after-save-hook
+              (lambda nil
+                (rust-test))))
+
+  (defun run-rust-test nil
+    (interactive)
+    (rust-test))
+
   :bind (("C-c C-c" . run-rust-test)))
 
 ;;   :hook ((c-mode c++-mode objc-mode) .
