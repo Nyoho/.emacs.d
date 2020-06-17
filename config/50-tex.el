b74f8ba5184d1c86125f226014d6a57afe923ddb
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-06-18 04:10:01 +0900

    leafed: latex-math-preview

--- config/50-tex.el
+++ config/50-tex.el
@@ -296,12 +296,9 @@ to [/]."
             ))
 
 
-;; latex-math-preview.el
-(autoload 'latex-math-preview-expression "latex-math-preview" nil t)
-(autoload 'latex-math-preview-insert-symbol "latex-math-preview" nil t)
-(autoload 'latex-math-preview-save-image-file "latex-math-preview" nil t)
-(autoload 'latex-math-preview-beamer-frame "latex-math-preview" nil t)
 
+(leaf latex-math-preview.el
+  :commands latex-math-preview-expression latex-math-preview-insert-symbol latex-math-preview-save-image-file latex-math-preview-beamer-frame)
 
 ;;
 ;; anything-math-symbols
