4d7d2bafb216a16b021f31fb52016d5abe1dd316
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-06-25 20:50:57 +0900

    Remove helm-interface for yasnippet

--- config/50-yasnippet.el
+++ config/50-yasnippet.el
@@ -48,25 +48,5 @@
 ;;   (buffer-substring-no-properties (car bounds) (cdr bounds)))
 ;; (bounds-of-thing-at-point 'symbol)
 
-
-;; helm interface
-;; http://d.hatena.ne.jp/syohex/20121207/1354885367
-(eval-after-load "helm"
-  '(progn
-     (defun my-yas/prompt (prompt choices &optional display-fn)
-       (let* ((names (loop for choice in choices
-                           collect (or (and display-fn (funcall display-fn choice))
-                                       choice)))
-              (selected (helm-other-buffer
-                         `(((name . ,(format "%s" prompt))
-                            (candidates . names)
-                            (action . (("Insert snippet" . (lambda (arg) arg))))))
-                         "*helm yas/prompt*")))
-         (if selected
-             (let ((n (position selected names :test 'equal)))
-               (nth n choices))
-           (signal 'quit "user quit!"))))
-     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
-
 ;; snippet-mode for *.yasnippet files
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
