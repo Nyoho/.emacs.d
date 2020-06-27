d0a946c241f316a2b989cbb6fb80c9575131470c
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-06-27 10:09:57 +0900

    Add scratch-comment for evaluation of emacs lisp

--- config/50-emacs-lisp.el
+++ config/50-emacs-lisp.el
@@ -1,1 +1,8 @@
 (add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
+
+(leaf scratch-comment
+  :ensure t
+  :bind ((lisp-interaction-mode-map
+          :package elisp-mode
+          ("C-u C-j" . scratch-comment-eval-sexp))))
+
