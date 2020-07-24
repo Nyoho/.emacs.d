a41a4178438e444c44ab3e072adfbec14a36221d
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-07-24 20:21:58 +0900

    Add face customization for ns

---
+++ config/cocoa-emacs-faces.el
@@ -0,0 +1,9 @@
+(leaf *macOS-face-customization
+  :when window-system
+  :config
+  :custom-face
+  (ns-marked-text-face  . '((t (:background "DeepPink4" :underline t))))
+  (ns-working-text-face . '((((background dark)) (:underline "gray80")))))
+
+
+
