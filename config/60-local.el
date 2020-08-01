d534733e5a73a45b10c8f25286140cdf61e8f03a
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-08-01 09:22:50 +0900

    Oops

--- config/60-local.el
+++ config/60-local.el
@@ -89,6 +89,7 @@
 
 ;; TODO: なんか候補は出るけどアクションの結果が出ない
 (leaf counsel-dash
+  :ensure t
   :bind
   (("C-c d" . counsel-dash-at-point))
   :custom ((counsel-dash-docsets-path . "~/Library/Application Support/Dash/DocSets/")
