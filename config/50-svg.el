59960d8f702a2a2384be2a8193a4df9256da7db6
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-08-28 23:54:07 +0900

    Add sample of svg

---
+++ config/50-svg.el
@@ -0,0 +1,21 @@
+(leaf *svg-exp
+  :disabled t
+  :config
+  (require 'svg)
+  (let ((svg (svg-create 77 17)))
+    ;; (svg-rectangle svg 3 0 71 17    :fill "blue")
+    ;; (svg-circle    svg 3   3   3    :fill "blue")
+    ;; (svg-rectangle svg 0   3   3 11 :fill "blue")
+    ;; (svg-circle    svg 3  14   3    :fill "blue")
+    ;; (svg-circle    svg 74  3   3    :fill "blue")
+    ;; (svg-rectangle svg 74  3   3 11 :fill "blue")
+    ;; (svg-circle    svg 74 14   3    :fill "blue")
+
+    (svg-rectangle svg 3 0 71 17 :fill "blue" :rx 3 :ry 3)
+
+    (svg-text svg "IMPORTANT"
+              :font-family "Menlo" :font-weight "light"
+              :font-size "12" :fill "white" :x 6 :y 13)
+    (insert-image (svg-image svg :ascent 'center)))
+  )
+
