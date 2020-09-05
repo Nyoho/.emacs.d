7af43d84811d0b32c0d7c6ed6abb6fa34d761594
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-09-05 13:00:30 +0900

    Use yasnippet in company

--- config/50-company.el
+++ config/50-company.el
@@ -25,6 +25,16 @@
   (leaf company-math
     :ensure t
     :after auctex org)
+
+  ;; yasnippetとの連携
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
   )
 
 (leaf company-box
