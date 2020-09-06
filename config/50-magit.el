e51300b0ae1305332e0f9adca2adab2d4e0ba291
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-09-06 13:50:29 +0900

    leafed: magit

--- config/50-magit.el
+++ config/50-magit.el
@@ -1,29 +1,20 @@
-(use-package magit
+(leaf magit
   :ensure t
   ;; :mode (("COMMIT_EDITMSG$" . magit-mode))
 
   ;; :after vc
+  :setq ((magit-auto-revert-mode . nil) ;; ?? 2015/4/16
+         (magit-last-seen-setup-instructions . "1.4.0")
+         ;; https://raw.githubusercontent.com/magit/magit/next/Documentation/RelNotes/1.4.0.txt
+         (magit-status-buffer-switch-function . 'switch-to-buffer)
+         (magit-log-edit-confirm-cancellation . nil)
+         (vc-handled-backends . '()))
   :config
   ;; (set-face-foreground 'magit-diff-add "green3")
   ;; (set-face-foreground 'magit-diff-del "red3")
 
   ;; (when (not window-system)
   ;;   (set-face-background 'magit-item-highlight "white"))
-
-  (setq magit-auto-revert-mode nil)
-  (setq magit-last-seen-setup-instructions "1.4.0")
-  (setq magit-status-buffer-switch-function 'switch-to-buffer)
-  ;; https://raw.githubusercontent.com/magit/magit/next/Documentation/RelNotes/1.4.0.txt
-
-  ;; ?? 2015/4/16
-  (setq magit-auto-revert-mode nil)
-  ;; ??
-  ;; (setq magit-last-seen-setup-instructions "1.4.0")
-
-  (defvar magit-log-edit-confirm-cancellation nil)
-
-  (setq vc-handled-backends '())
   ;; (eval-after-load "vc"
   ;;   '(remove-hook 'find-file-hooks 'vc-find-file-hook))
   )
-
