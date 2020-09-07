9a41fc9c623dc1f77794f0ac578307366cbeaf57
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-09-07 10:33:24 +0900

    leafed: sage

--- config/50-sage.el
+++ config/50-sage.el
@@ -1,16 +1,15 @@
 ;; https://github.com/sagemath/sage-shell-mode
 
-(use-package sage-shell-mode
+(leaf sage-shell-mode
   :ensure t
+  ;; Turn on eldoc-mode in Sage terminal and in Sage source files
+  :hook ((sage-shell-mode-hook . eldoc-mode)
+         (sage-shell:sage-mode-hook . eldoc-mode))
   :config
   (setq sage-shell:use-prompt-toolkit nil)
   (setq sage-shell:sage-root "/Applications/SageMath-8.6.app")
   ;; Run SageMath by M-x run-sage instead of M-x sage-shell:run-sage
   (sage-shell:define-alias)
-
-  ;; Turn on eldoc-mode in Sage terminal and in Sage source files
-  (add-hook 'sage-shell-mode-hook #'eldoc-mode)
-  (add-hook 'sage-shell:sage-mode-hook #'eldoc-mode)
   )
 
 ;; これはまだ
