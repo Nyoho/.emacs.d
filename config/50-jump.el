bdeeefefa62c9f99023f918a246de275d2d67e52
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-09-08 16:13:25 +0900

    Move to jump.el

--- config/50-jump.el
+++ config/50-jump.el
@@ -2,6 +2,21 @@
 ;; jump 系
 ;;
 
+;;
+;; ace-jump-mode
+;;
+;; ヒント文字に使う文字を指定する
+(setq ace-jump-mode-move-keys
+      (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
+;; ace-jump-word-modeのとき文字を尋ねないようにする
+(setq ace-jump-word-mode-use-query-char nil)
+
+;; (global-set-key (kbd "C-;") 'ace-jump-char-mode)
+;; (global-set-key (kbd "C-;") 'ace-jump-mode)
+;; (global-set-key (kbd "C-;") 'ace-jump-word-mode)
+;; (global-set-key (kbd "C-M-;") 'ace-jump-line-mode)
+
+
 ;; ace-jump-mode よりjaunteのほういい気がする
 ;; (use-package jaunte)
 ;; (global-set-key (kbd "C-;") 'jaunte)
