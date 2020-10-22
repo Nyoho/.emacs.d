76cdad1366ea6bd37f6d1c8a7e83197830949b92
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-10-22 19:03:35 +0900

    Update projectile setting

--- config/50-projectile.el
+++ config/50-projectile.el
@@ -1,14 +1,14 @@
 (leaf projectile
   :ensure t
-  :require t
   :setq ((projectile-completion-system . 'ivy))
   :bind
   (projectile-mode-map
    ("s-p"   . projectile-command-map)
    ("C-c p" . projectile-command-map))
   :config
-  (leaf counsel-projectile
-    :ensure t
-    :bind
-    ("M-o p" . counsel-projectile-switch-project))
   (projectile-mode))
+
+(leaf counsel-projectile
+  :ensure t
+  :bind
+  ("M-o p" . counsel-projectile-switch-project))
