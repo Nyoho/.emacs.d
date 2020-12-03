64842f77ccb5427661c3737be7f7e6fa9be99dd5
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-12-04 07:20:37 +0900

    Oops

--- config/50-org.el
+++ config/50-org.el
@@ -379,12 +379,11 @@
   ;; (setq org-format-latex-options
   ;;       '(:foreground default :background default :scale 2.0 :html-foreground "Black" :html-background "Transparent" :html-scale 2.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
 
-  (setq org-preview-latex-process-alist
+  (setq org-preview-latex-process-alist-nil
         '((dvipng :programs ("latex" "dvipng") :description "dvi > png" :message "you need to install the programs: latex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust (1.0 . 1.0) :latex-compiler ("/Library/TeX/texbin/platex -interaction nonstopmode -output-directory %o %f") :image-converter ("/Library/TeX/texbin/dvipng -fg %F -bg %B -D %D -T tight -o %O %f"))
           (dvisvgm :programs ("latex" "dvisvgm") :description "dvi > svg" :message "you need to install the programs: latex and dvisvgm." :use-xcolor t :image-input-type "dvi" :image-output-type "svg" :image-size-adjust (1.7 . 1.5) :latex-compiler ("/Library/TeX/texbin/platex -interaction nonstopmode -output-directory %o %f") :image-converter ("/Library/TeX/texbin/dvisvgm %f -n -b min -c %S -o %O"))
           (imagemagick :programs ("latex" "convert") :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :use-xcolor t :image-input-type "pdf" :image-output-type "png" :image-size-adjust (1.0 . 1.0) :latex-compiler ("/Library/TeX/texbin/lualatex -interaction nonstopmode -output-directory %o %f") :image-converter ("convert -density %D -trim -antialias %f -quality 100 %O")))
         )
-  )
 
   ;; Beamer presentations using the new export engine
   ;; https://orgmode.org/worg/exporters/beamer/ox-beamer.html#export-filters
