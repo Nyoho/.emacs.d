792a9ea793388cdad948e82b99a8f66260b29177
北䑓如法 <algebraicallyClosedField@gmail.com>
2020-12-17 20:00:32 +0900

    flycheck of textlint

--- config/30-flycheck.el
+++ config/30-flycheck.el
@@ -18,6 +18,18 @@
   ;; flycheck-pos-tip
   ;; (flycheck-pos-tip-mode)
   ;; (setq flycheck-pos-tip-timeout 30)
+
+  (flycheck-define-checker textlint
+    "A linter called textlint."
+    :command ("textlint" "--format" "unix" source)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+              (id (one-or-more (not (any " "))))
+              (message (one-or-more not-newline)
+                       (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+              line-end))
+    :modes (text-mode markdown-mode gfm-mode review-mode))
+  (add-to-list 'flycheck-checkers 'textlint)
   )
 
 ;; Local Variables:
