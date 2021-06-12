8177a3025b393a7b4df021134f335e1672776b11
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-06-12 17:19:14 +0900

    leafed: session

--- config/50-session.el
+++ config/50-session.el
@@ -1,18 +1,16 @@
 ;;
 ;; session.el -- カーソル位置とか kill-ring とか覚えておいてくれて便利
 ;;
-(use-package session
+(leaf session
   :ensure t
-  :config
-  (add-hook 'after-init-hook 'session-initialize)
-  (setq session-save-print-spec '(t nil 40000))
-  (setq session-initialize '(de-saveplace session keys menus places)
-        session-globals-include '((kill-ring 50)
-                                  (session-file-alist 500 t)
-                                  (file-name-history 65536))) ;; これがないと file-name-history に500個保存する前に max-string に達する
-  (setq session-globals-max-string 100000)
-  ;; デフォルトでは30
-  (setq history-length 100000)
-  (setq session-save-file "~/.session")
-  (setq session-name-disable-regexp "/COMMIT_EDITMSG$")
-  (add-hook 'after-init-hook 'session-initialize))
+  :hook ((after-init-hook . session-initialize))
+  :setq
+  ((session-save-print-spec . '(t nil 40000))
+   (session-initialize . '(de-saveplace session keys menus places))
+   (session-globals-include . '((kill-ring 50)
+				                (session-file-alist 500 t)
+				                (file-name-history 65536)))
+   (session-globals-max-string . 100000)
+   (history-length . 100000)
+   (session-save-file . "~/.session")
+   (session-name-disable-regexp . "/COMMIT_EDITMSG$")))
