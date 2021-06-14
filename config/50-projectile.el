2af92576f24c8b76ea94ef134b970353f095349f
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-06-14 10:25:59 +0900

    Remove counsel-projectile

--- config/50-projectile.el
+++ config/50-projectile.el
@@ -1,14 +1,9 @@
 (leaf projectile
   :ensure t
-  :setq ((projectile-completion-system . 'ivy))
+  ;; :setq ((projectile-completion-system . 'ivy))
   :bind
   (projectile-mode-map
    ("s-p"   . projectile-command-map)
    ("C-c p" . projectile-command-map))
-  :config
+  :init
   (projectile-mode))
-
-(leaf counsel-projectile
-  :ensure t
-  :bind
-  ("M-o p" . counsel-projectile-switch-project))
