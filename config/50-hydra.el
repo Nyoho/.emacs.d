cb8a2672e11d59918aa654a3fbc6a9a2e7118208
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-07-22 10:17:07 +0900

    Update hydra/move
    from https://ladicle.com/post/config/

--- config/50-hydra.el
+++ config/50-hydra.el
@@ -1,5 +1,47 @@
 (leaf hydra
-      :ensure t)
+  :ensure t
+  :bind
+  ("C-z" . hydra-viewer/body)
+  ("C-c ." . help/hydra/timestamp/body)
+  :config
+  (with-eval-after-load 'hydra
+    (defhydra hydra-viewer (:color pink :hint nil)
+      "
+                                                                        ╔════════╗
+   Char/Line^^^^^^  Word/Page^^^^^^^^  Line/Buff^^^^   Paren                              ║ Window ║
+  ──────────────────────────────────────────────────────────────────────╨────────╜
+       ^^_k_^^          ^^_u_^^          ^^_g_^^       _(_ ← _y_ → _)_
+       ^^^↑^^^          ^^^↑^^^          ^^^↑^^^       _,_ ← _/_ → _._
+   _h_ ← _d_ → _l_  _H_ ← _D_ → _L_  _a_ ← _K_ → _e_
+       ^^^↓^^^          ^^^↓^^^          ^^^↓^
+       ^^_j_^^          ^^_n_^^          ^^_G_
+  ╭──────────────────────────────────────────────────────────────────────────────╯
+                           [_q_]: quit, [_<SPC>_]: center
+          "
+      ("j" scroll-down-in-place)
+      ("k" scroll-up-in-place)
+      ("l" forward-char)
+      ("d" delete-char)
+      ("h" backward-char)
+      ("L" forward-word)
+      ("H" backward-word)
+      ("u" scroll-up-command)
+      ("n" scroll-down-command)
+      ("D" delete-word-at-point)
+      ("a" mwim-beginning-of-code-or-line)
+      ("e" mwim-end-of-code-or-line)
+      ("g" beginning-of-buffer)
+      ("G" end-of-buffer)
+      ("K" kill-whole-line)
+      ("(" backward-list)
+      (")" forward-list)
+      ("y" yank-inner-sexp)
+      ("." backward-forward-next-location)
+      ("," backward-forward-previous-location)
+      ("/" avy-goto-char :exit t)
+      ("<SPC>" recenter-top-bottom)
+      ("q" nil))
+    ))
 
 (defhydra hydra/move ()
   "move"
@@ -10,9 +52,6 @@
   ("SPC" scroll-up-command "down")
   ("<backspace>" scroll-down-command "up")
   ("." hydra-repeat "repeat"))
-
-(global-set-key (kbd "C-z") #'hydra/move/body)
-(global-set-key (kbd "C-c .") #'help/hydra/timestamp/body)
 
 (defhydra help/hydra/timestamp (:color blue :hint none)
   "
