b720333e0138b4d80787b6cc13ff5013d5f9e031
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-07-23 22:30:41 +0900

    [hydra] refactoring

--- config/50-hydra.el
+++ config/50-hydra.el
@@ -63,56 +63,54 @@ _3_.  ?s?          (Org Mode: by _s_elect)
       ("1" help/insert-timestamp)
       ("2" help/org-time-stamp-with-seconds-now)
       ("3" org-time-stamp))
+
+    (defun help/show-my-date ()
+      "Produces and show date and time in preferred format."
+      (interactive)
+      (message (format-time-string "%Y-%m-%d (%a.) W:%W @%H:%M"))
+      (hydra-keyboard-quit))
+
+    (defun help/insert-currenttime ()
+      "Produces and inserts the current time."
+      (interactive)
+      (insert (format-time-string "%H:%M")))
+
+    (defun help/insert-week ()
+      "Produces and inserts the week number."
+      (interactive)
+      (insert (format-time-string "%W")))
+
+    (defun help/insert-month-and-day ()
+      "Inserts a month and day pair in 4-degits."
+      (interactive)
+      (insert (format-time-string "%m%d")))
+
+    (defun help/insert-datestamp ()
+      "Produces and inserts a partial ISO 8601 format timestamp."
+      (interactive)
+      (insert (format-time-string "%F")))
+
+    (defun help/insert-timestamp ()
+      "Inserts a full ISO 8601 format timestamp."
+      (interactive)
+      (insert (help/get-timestamp)))
+
+    (defun help/org-time-stamp-with-seconds-now ()
+      (interactive)
+      (let ((current-prefix-arg '(16)))
+        (call-interactively 'org-time-stamp)))
+
+    (defun help/get-timestamp ()
+      "Produces a full ISO 8601 format timestamp."
+      (interactive)
+      (let* ((timestamp-without-timezone (format-time-string "%Y-%m-%dT%T"))
+             (timezone-name-in-numeric-form (format-time-string "%z"))
+             (timezone-utf-offset
+              (concat (substring timezone-name-in-numeric-form 0 3)
+                      ":"
+                      (substring timezone-name-in-numeric-form 3 5)))
+             (timestamp (concat timestamp-without-timezone
+                                timezone-utf-offset)))
+        timestamp))
     ))
 
-(defhydra hydra/move ()
-  "move"
-  ("f" forward-char "right")
-  ("b" backward-char "left")
-  ("n" next-line "down")
-  ("p" previous-line "up")
-  ("SPC" scroll-up-command "down")
-  ("<backspace>" scroll-down-command "up")
-  ("." hydra-repeat "repeat"))
-
-(defun help/show-my-date ()
-  "Produces and show date and time in preferred format."
-  (interactive)
-  (message (format-time-string "%Y-%m-%d (%a.) W:%W @%H:%M"))
-  (hydra-keyboard-quit))
-(defun help/insert-currenttime ()
-  "Produces and inserts the current time."
-  (interactive)
-  (insert (format-time-string "%H:%M")))
-(defun help/insert-week ()
-  "Produces and inserts the week number."
-  (interactive)
-  (insert (format-time-string "%W")))
-(defun help/insert-month-and-day ()
-  "Inserts a month and day pair in 4-degits."
-  (interactive)
-  (insert (format-time-string "%m%d")))
-(defun help/insert-datestamp ()
-  "Produces and inserts a partial ISO 8601 format timestamp."
-  (interactive)
-  (insert (format-time-string "%F")))
-(defun help/insert-timestamp ()
-  "Inserts a full ISO 8601 format timestamp."
-  (interactive)
-  (insert (help/get-timestamp)))
-(defun help/org-time-stamp-with-seconds-now ()
-  (interactive)
-  (let ((current-prefix-arg '(16)))
-    (call-interactively 'org-time-stamp)))
-(defun help/get-timestamp ()
-  "Produces a full ISO 8601 format timestamp."
-  (interactive)
-  (let* ((timestamp-without-timezone (format-time-string "%Y-%m-%dT%T"))
-         (timezone-name-in-numeric-form (format-time-string "%z"))
-         (timezone-utf-offset
-          (concat (substring timezone-name-in-numeric-form 0 3)
-                  ":"
-                  (substring timezone-name-in-numeric-form 3 5)))
-         (timestamp (concat timestamp-without-timezone
-                            timezone-utf-offset)))
-    timestamp))
