3ea01cfba06318d756924d109750afd6ffea4192
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-08-16 22:01:24 +0900

    Update early-init

--- early-init.el
+++ early-init.el
@@ -2,6 +2,8 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 (setq frame-inhibit-implied-resize t)
 
-(setq tool-bar-mode t)
+(setq tool-bar-mode nil)
 (setq ns-pop-up-frames nil)
 (setq org-roam-v2-ack t)
+
+(setq inhibit-startup-screen t)
