176f14980024b20f9566ca9dc834f481293f6203
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-04-07 07:34:11 +0900

    [svg] define function

--- config/50-svg.el
+++ config/50-svg.el
@@ -1,8 +1,9 @@
 (leaf *svg-exp
-  :disabled t
+  :if (window-system)
   :config
   ;; (image-type-available-p 'svg) => if 't' then ok
   (require 'svg)
+  (defun sample-make-tag ()
   (let ((svg (svg-create 77 17)))
     ;; (svg-rectangle svg 3 0 71 17    :fill "blue")
     ;; (svg-circle    svg 3   3   3    :fill "blue")
@@ -18,5 +19,6 @@
               :font-family "Menlo" :font-weight "light"
               :font-size "12" :fill "white" :x 6 :y 13)
     (insert-image (svg-image svg :ascent 'center)))
+    )
   )
 
