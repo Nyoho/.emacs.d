592ffd5a690dd9779dc54abe05217cc2a2850f2c
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-04-08 07:04:42 +0900

    [svg] Add svg-tag-mode

--- config/50-svg.el
+++ config/50-svg.el
@@ -22,3 +22,28 @@
     )
   )
 
+
+(leaf svg-tag-mode
+  :ensure t
+  :require t
+  :hook (;;(prog-mode-hook . svg-tag-mode)
+         (org-mode-hook . svg-tag-mode))
+  :config
+  (setq svg-tag-tags
+        '(
+          ("DONE" . ((lambda (tag) (svg-tag-make "DONE" :face 'org-done :margin 0))))
+          ("FIXME" . ((lambda (tag) (svg-tag-make "FIXME" :face 'org-todo :inverse t :margin 0))))
+          ("\\/\\/\\W?MARK:\\|MARK:" . ((lambda (tag) (svg-tag-make "MARK" :face 'font-lock-doc-face :inverse t :margin 0 :crop-right t))))
+          ("MARK:\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'font-lock-doc-face :crop-left t))))
+          
+          ("\\/\\/\\W?swiftlint:disable" . ((lambda (tag) (svg-tag-make "disabled lint rule:" :face 'org-done :inverse t :margin 0 :crop-right t))))
+          ("swiftlint:disable\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'org-done :crop-left t))))
+          
+          ;; TODOS
+          ("\\/\\/\\W?TODO\\|TODO" . ((lambda (tag) (svg-tag-make "TODO" :face 'org-todo :inverse t :margin 0 :crop-right nil))))
+          ;; ("TODO\\(.*\\)" . ((lambda (tag) (svg-tag-make tag :face 'org-todo :crop-left t))))
+
+          ;; tag in org-mode
+          ("\\s-\\(:[a-zA-Z]+:\\)$" . ((lambda (tag) (svg-tag-make tag :beg 1 :end -1))))
+          ;; ("[[:space:]]+\\(:[a-zA-Z]+:\\)$" . ((lambda (tag) (svg-tag-make tag :beg 1 :end -1))))
+          )))
