2ed063211ce044d2e8b419b38f106fc9776e5f74
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-05-07 17:04:13 +0900

    [sudden-death] Change melpa to el-get

--- config/50-sudden-death.el
+++ config/50-sudden-death.el
@@ -2,4 +2,7 @@
 ;; sudden-death.el
 ;;
 (leaf sudden-death
-  :ensure t)
+  :el-get yewton/sudden-death.el)
+
+;; melpaから削除されていた 20 Apr 2021
+;; https://github.com/melpa/melpa/commit/39bc92a1cbbfae80a82cae22d8f13931c6a76352
