2b37a947e1744453728e9c642322f857f45469e9
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-05-17 20:21:44 +0900

    Add tree-sitter
    これを見ながら https://zenn.dev/hyakt/articles/6ff892c2edbabb

---
+++ config/50-tree-sitter.el
@@ -0,0 +1,15 @@
+(leaf tree-sitter
+  :ensure (t tree-sitter-langs)
+  :require tree-sitter-langs
+  :mode
+  (("\\.tsx$" . typescript-tsx-mode))
+  :init
+  (define-derived-mode typescript-tsx-mode typescript-mode "TSX")
+  :hook
+  (tree-sitter-after-on-hook . tree-sitter-hl-mode)
+  :config
+  (global-tree-sitter-mode)
+
+  (tree-sitter-require 'tsx)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx)))
+
