28ad2368855e47c233d575fd5b4244652c819fb7
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-06-09 20:59:44 +0900

    Remove require

--- config/50-tree-sitter.el
+++ config/50-tree-sitter.el
@@ -1,6 +1,5 @@
 (leaf tree-sitter
   :ensure (t tree-sitter-langs)
-  :require tree-sitter-langs
   :hook
   (tree-sitter-after-on-hook . tree-sitter-hl-mode)
   :config
@@ -9,3 +8,6 @@
   (tree-sitter-require 'tsx)
   (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx)))
 
+(leaf tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
