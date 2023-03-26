b54848dec1dd120b85088ce496f24898d49d1c27
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-03-26 22:24:57 +0900

    Modify setting of typescript-mode

--- config/50-typescript.el
+++ config/50-typescript.el
@@ -2,25 +2,13 @@
   :ensure t
   :mode 
   (("\\.ts\\'" . typescript-mode)
-   ("\\.tsx\\'" . typescript-tsx-mode))
-  :preface
-  (define-derived-mode typescript-tsx-mode typescript-mode "TSX"
-    (setq-local indent-line-function 'rjsx-indent-line))
-  :bind 
-  (typescript-tsx-mode-map
-   ("<" . rjsx-electric-lt)
-   (">" . rjsx-electric-gt))
+   ("\\.tsx\\'" . tsx-ts-mode))
   :custom
   (typescript-indent-level . 2)
   :setq 
   (typescript-tsx-indent-offset . 2)
   :hook
   (typescript-mode-hook . subword-mode)
-  (typescript-tsx-mode-hook . rjsx-minor-mode)
-  (typescript-tsx-mode-hook . tree-sitter-hl-mode)
-  (typescript-tsx-mode-hook . (lambda ()
-                                (rjsx-minor-mode)
-                                (tree-sitter-hl-mode)))
   :config)
 
 (leaf tide
