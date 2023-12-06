6dbfc332828bada3948ac48973514696e6eb178a
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-12-06 23:02:31 +0900

    [org] Modify todo template

--- config/50-org.el
+++ config/50-org.el
@@ -90,7 +90,7 @@
         '(("t" "Todo" entry
            (file+headline (lambda () (concat org-directory "/agenda/inbox.org")) "Inbox")
            ;; (file (lambda () (concat org-directory "/agenda/inbox.org")))
-           "* TODO %^{やること(「〜する」)} %^g\n%?\n")
+           "* TODO %^{やること(「〜する」)}\n%?\n")
           ("U" "Note" entry
            (file+headline (lambda () (concat org-directory "/notes.org" "")) "")
            "* %U %^{トピックス} %^g %i%? %a")
