ff24ae81f7ebbbc5b8269b920118de77c6a03523
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-02-29 23:03:27 +0900

    Modify debug file for projectile

--- .debug/projectile/init.el
+++ .debug/projectile/init.el
@@ -28,5 +28,11 @@
 
 (leaf projectile
   :ensure t
+  :bind
+  (projectile-mode-map
+   ("s-p"   . projectile-command-map)
+   ("C-c p" . projectile-command-map))
   :init
-  (projectile-mode))
+  (projectile-mode +1)
+  :custom
+  (projectile-switch-project-action . #'projectile-dired))
