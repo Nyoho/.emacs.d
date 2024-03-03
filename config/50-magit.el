cd7696c842258d2c1d81cb9552e36d0bcc038664
北䑓如法 <algebraicallyClosedField@gmail.com>
2024-03-03 10:08:23 +0900

    [magit] Customize magit-status-sections-hook

--- config/50-magit.el
+++ config/50-magit.el
@@ -19,6 +19,30 @@
   ;;   (set-face-background 'magit-item-highlight "white"))
   ;; (eval-after-load "vc"
   ;;   '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+
+  (setq magit-status-sections-hook
+        '(magit-insert-status-headers
+          magit-insert-merge-log
+          magit-insert-rebase-sequence
+          magit-insert-am-sequence
+          magit-insert-sequencer-sequence
+          magit-insert-bisect-output
+          magit-insert-bisect-rest
+          magit-insert-bisect-log
+          magit-insert-untracked-files
+          magit-insert-unstaged-changes
+          magit-insert-staged-changes
+          magit-insert-stashes
+          magit-insert-unpushed-to-pushremote
+          magit-insert-unpushed-to-upstream-or-recent
+          magit-insert-unpulled-from-pushremote
+          magit-insert-unpulled-from-upstream))
+
+  ;; 非常に遅いリポジトリのとき一時的に次を試す。
+  ;; (setq magit-diff-highlight-trailing  nil
+  ;;       magit-diff-paint-whitespace    nil
+  ;;       magit-diff-highlight-hunk-body nil)
+
   :custom
   ((magit-log-margin . '(t "%Y-%m-%d %H:%M:%S" magit-log-margin-width t 12))))
 
