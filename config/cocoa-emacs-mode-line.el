36c2267e2c0fd200a0b579859881028a90dca79d
北䑓如法 <algebraicallyClosedField@gmail.com>
2025-07-17 19:08:04 +0900

    Refactor mode-line customization

--- config/cocoa-emacs-mode-line.el
+++ config/cocoa-emacs-mode-line.el
@@ -2,32 +2,38 @@
 ;; Set faces
 ;;
 
-(leaf modeline-customization
-  :when window-system
-  :config
+(defvar after-load-theme-hook nil
+  "Hooks run after a color theme is loaded using `load-theme'.")
+
+(defun run-after-load-theme-hook (&rest _)
+  (run-hooks 'after-load-theme-hook))
+
+(advice-add 'enable-theme :after #'run-after-load-theme-hook)
+
+(defun set-my-mode-line ()
+  (set-face-attribute 'mode-line nil
+                      :family "DIN Condensed"
+                      :weight 'bold
+                      :height 1.0
+                      :foreground "white"
+                      :background "MediumPurple2")
   
-  ;; (set-face-attribute 'mode-line nil :family "Optima" :height 0.95)
-  (set-face-attribute 'mode-line nil :family "DIN Condensed" :weight 'bold :height 1.0)
-  ;; (set-face-attribute 'mode-line nil :family "Avenir Next Condensed" :weight 'normal :height 1.0)
-  ;; (set-face-attribute 'mode-line nil :family "Avenir Next Condensed" :height 1.0)
-  (set-face-attribute 'mode-line-inactive nil :family "DIN Condensed" :height 1.0)
-  (set-face-attribute 'mode-line-inactive nil :family "DIN Condensed" :weight 'bold :height 1.0)
+  (set-face-attribute 'mode-line-inactive nil
+                      :family "DIN Condensed"
+                      :weight 'bold
+                      :height 1.0
+                      :foreground "gray70"
+                      :background "gray20")
 
   (setq-default mode-line-format
                 (cons (propertize "\u200b" 'display '((raise 0.5) (height 0.0))) mode-line-format))
 
-  ;; '(mode-line-inactive ((t (:background "#262626" :foreground "#888" :box nil :height 1.1 :family "Optima"))))
+  (set-face-background 'region "DeepPink"))
 
-  ;; '(mode-line ((t (:background "#32649c" :foreground "white" :box nil :height 1.1 :width condensed :family "Optima"))))
-  ;; '(mode-line-inactive ((t (:background "#262626" :foreground "#888" :box nil :height 1.1 :family "Optima"))))
-
-  ;; (set-face-foreground 'mode-line "white")
-  ;; (set-face-foreground 'mode-line "black")
-  ;; (set-face-background 'mode-line "MediumPurple2")
-  ;; (set-face-background 'mode-line-highlight "MediumPurple2")
-  ;; (set-face-background 'region "LightSteelBlue2")
-  (set-face-background 'region "DeepPink")
-  (set-face-foreground 'mode-line-inactive "gray11")
-  (set-face-background 'mode-line-inactive "gray20")
-  (set-face-foreground 'mode-line-inactive "gray70")
-  )
+(leaf *modeline-customization*
+  :when window-system
+  :hook
+  (after-load-theme-hook . set-my-mode-line)
+  (after-init-hook . set-my-mode-line)
+  :config
+  (set-my-mode-line))
